e08decc333682539e495d420f186c8e7df20ce7e
prestonlindhardt <46542413+prestonlindhardt@users.noreply.github.com>
2019-10-03 14:57:20 -0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,32 @@
+// var options = {
+   // version: 1.0,
+    //data: {
+      //  updated: "9/30/2019",
+        //items: [
+        //{// pie chart for survival rate
+        //chart:{
+          //  type: "pie"
+        //},
+          //  labels: ["Survived", "Died"],
+			//	series: [342, 549],
+			//	title: {
+              //      Text: "survival Rate",
+                //    align: "center",
+                  //  style: {
+                    //    fontSize: "18px",
+                      //  color: "#595959",
+                        //fontFamily: "Poppins"
+                    //}
+    //}, //end of pie chart
+            
+      //  ]
+
+        
+    //}
+//};
+
+// var chart = new ApexCharts(document.querySelector("#chart0"), options.data.items[0]);
+
+// chart.render();
+
+
--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 // javascript
+// Variables
+var cont_sidebar = document.getElementById("cont_sidebar");
+var btn_settings = document.getElementById("cont_settings");
+console.log(cont_sidebar);
+console.log(btn_settings); //not functioning. not showing up in console
+
+//event listeners
+btn_settings.addEventListener("click", function(){
+    console.log("User clicked on btn_settings");
+    cont_sidebar.classList.toggle("active");
+});
+
+cont_sidebar.addEventListener("dblclick", function(){
+    console.log("User double clicked on cont_sidebar");
+    cont_sidebar.classList.toggle("active");
+});
+
+
